19b5a1070184e5b98a2d5947cf3f52b8728e5cb8
luvalenciaq <luisamariavalenciaq@gmail.com>
2025-12-19 16:14:43 -0600

    initialize spring boot bff

---
+++ src/test/java/com/flightontime/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightontime.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
