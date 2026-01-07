685c72224feba1da3e343c51885b8f8b99492b8a
Eliana Ḿendez <eliamargi@gmail.com>
2026-01-07 01:02:11 -0500

    see ajusta PredictionClientService para retorno de 2 decimales

--- src/main/java/com/flightontime/bff/dto/PredictionResponseDTO.java
+++ src/main/java/com/flightontime/bff/dto/PredictionResponseDTO.java
@@ -1,9 +1,10 @@
 package com.flightontime.bff.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "DTO con la respuesta de la predicción")
 public record PredictionResponseDTO(
                 @Schema(description = "Previsión del vuelo (ej. 'Puntual', 'Retrasado')", example = "Puntual") String prevision,
-                @Schema(description = "Probabilidad de la predicción", example = "0.18252110481262207") double probabilidad) {
+                @Schema(description = "Probabilidad de la predicción", example = "0.24") double probabilidad) {
 }
