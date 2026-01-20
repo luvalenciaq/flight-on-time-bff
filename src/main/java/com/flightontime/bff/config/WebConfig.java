c5bfc4d84a244e048e929822eb2ea3273b51ca13
maha <marcotaku@gmail.com>
2026-01-19 21:11:36 -0300

    se configura cors para conectar front

---
+++ src/main/java/com/flightontime/bff/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.flightontime.bff.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("*") // En producción restringir esto
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
